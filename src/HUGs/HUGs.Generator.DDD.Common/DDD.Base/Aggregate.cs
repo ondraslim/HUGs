eca0056c2bdb2fc961a9a12c710980d209928a61
Ondřej Slimák <ondrej.slimak@riganti.cz>
2021-07-12 21:46:23 +0200

    added aggregate class

---
+++ src/HUGs/HUGs.Generator.DDD.Common/DDD.Base/Aggregate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HUGs.Generator.DDD.Common.DDD.Base
+{
+    public abstract class Aggregate<TKey> : Entity<TKey>, IAggregate<TKey>
+    {
+        protected Aggregate()
+        {
+            Events = new List<object>();
+        }
+
+        public string Etag { get; protected set; }
+
+        public void RegenerateEtag()
+        {
+            Etag = Guid.NewGuid().ToString();
+        }
+
+        private List<object> Events { get; set; }
+
+        public abstract void CheckState();
+
+        public override bool Equals(object other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return this == other;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        protected void AddEvent(object @event)
+        {
+            Events ??= new List<object>();
+            Events.Add(@event);
+        }
+
+        public IEnumerable<object> GetEvents()
+        {
+            return Events ?? new List<object>();
+        }
+    }
+}
---
+++ src/HUGs/HUGs.Generator.DDD.Common/DDD.Base/IAggregate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace HUGs.Generator.DDD.Common.DDD.Base
+{
+    public interface IAggregate<TKey> : IEntity<TKey>
+    {
+        void CheckState();
+        public string Etag { get; }
+        void RegenerateEtag();
+        bool Equals(object other);
+        int GetHashCode();
+        public IEnumerable<object> GetEvents();
+    }
+}
