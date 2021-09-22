04893d7aab3c88eaa917b07157b82fa3b4e7d67e
Ondřej Slimák <ondrej.slimak@riganti.cz>
2021-09-22 20:18:09 +0200

    add test results

---
+++ src/HUGs/HUGs.Generator.DDD.Tests/TestResults/ValueObjects/DddValueObjectSourceGeneratorTests.ValidSimpleValueObjectSchema2_GeneratorRun_GeneratesCorrectValueObject.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using HUGs.Generator.DDD.BaseModels;
+
+namespace HUGs.DDD.Generated.ValueObject
+{
+    public partial class Simple2 : HUGs.Generator.DDD.BaseModels.ValueObject
+    {
+        public string Text { get; }
+
+        public int Number { get; }
+
+        public Simple2(string Text, int Number)
+        {
+            this.Text = Text;
+            this.Number = Number;
+        }
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Text;
+            yield return Number;
+        }
+    }
+}
