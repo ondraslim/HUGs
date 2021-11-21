96c2934ec005dd3485b85b1c5787301439ee5f91
Ondřej Slimák <ondrej.slimak@riganti.cz>
2021-11-21 10:55:34 +0100

    add missing test result

---
+++ src/HUGs/HUGs.Generator.DDD.IntegrationTests/TestResults/EntitySourceGeneratorTests.SimpleSchema_GeneratedCorrectly-CountryMapper.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using HUGs.Generator.DDD.Framework.BaseModels;
+using HUGs.Generator.DDD.Framework.Mapping;
+using HUGs.DDD.Generated.Entity;
+using HUGs.DDD.Generated.Aggregate;
+using HUGs.DDD.Generated.ValueObject;
+using HUGs.DDD.Generated.Enumeration;
+
+namespace HUGs.DDD.Generated.DbEntity
+{
+    public class CountryMapper : DbEntityMapper<Country, CountryDbEntity>
+    {
+        public CountryMapper(IDbEntityMapperFactory factory): base(factory)
+        {
+        }
+
+        public override CountryDbEntity ToDbEntity(Country obj)
+        {
+            return new CountryDbEntity
+            {
+            	Name = obj.Name
+            };
+        }
+
+        public override Country ToDddObject(CountryDbEntity obj)
+        {
+            return new Country
+            (
+            	obj.Name
+            );
+        }
+    }
+}
