11dcf3385c5bd1b0cca84f4be4da2ad338ea8aab
Ondřej Slimák <ondrej.slimak@riganti.cz>
2021-11-21 10:44:36 +0100

    add complex test results

---
+++ src/HUGs/HUGs.Generator.DDD.IntegrationTests/TestResults/DddSourceGeneratorTests.ComplexDddModel_GeneratedCorrectly-Address.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using HUGs.Generator.DDD.Framework.BaseModels;
+using HUGs.Generator.DDD.Framework.Mapping;
+using HUGs.DDD.Generated.Entity;
+using HUGs.DDD.Generated.Aggregate;
+using HUGs.DDD.Generated.ValueObject;
+using HUGs.DDD.Generated.Enumeration;
+
+namespace HUGs.DDD.Generated.ValueObject
+{
+    public partial class Address : HUGs.Generator.DDD.Framework.BaseModels.ValueObject
+    {
+        public string Street { get; }
+
+        public string? Street2 { get; }
+
+        public string City { get; }
+
+        public string Zip { get; }
+
+        public CountryId CountryId { get; }
+
+        public Address(string Street, string? Street2, string City, string Zip, CountryId CountryId)
+        {
+            this.Street = Street;
+            this.Street2 = Street2;
+            this.City = City;
+            this.Zip = Zip;
+            this.CountryId = CountryId;
+            OnInitialized();
+        }
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Street;
+            yield return Street2;
+            yield return City;
+            yield return Zip;
+            yield return CountryId;
+        }
+
+        partial void OnInitialized();
+    }
+}
---
+++ src/HUGs/HUGs.Generator.DDD.IntegrationTests/TestResults/DddSourceGeneratorTests.ComplexDddModel_GeneratedCorrectly-AddressMapper.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using HUGs.Generator.DDD.Framework.BaseModels;
+using HUGs.Generator.DDD.Framework.Mapping;
+using HUGs.DDD.Generated.Entity;
+using HUGs.DDD.Generated.Aggregate;
+using HUGs.DDD.Generated.ValueObject;
+using HUGs.DDD.Generated.Enumeration;
+
+namespace HUGs.DDD.Generated.DbEntity
+{
+    public class AddressMapper : DbEntityMapper<Address, AddressDbEntity>
+    {
+        public AddressMapper(IDbEntityMapperFactory factory): base(factory)
+        {
+        }
+
+        public override AddressDbEntity ToDbEntity(Address obj)
+        {
+            return new AddressDbEntity
+            {
+            	Street = obj.Street,
+            	Street2 = obj.Street2,
+            	City = obj.City,
+            	Zip = obj.Zip,
+            	CountryId = MapDbEntityId(obj.CountryId)
+            };
+        }
+
+        public override Address ToDddObject(AddressDbEntity obj)
+        {
+            return new Address
+            (
+            	obj.Street,
+            	obj.Street2,
+            	obj.City,
+            	obj.Zip,
+            	MapDddObjectId(obj.CountryId)
+            );
+        }
+    }
+}
