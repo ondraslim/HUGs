907222fd52f461f61c02856bd4aeaf5de44793f3
Ondřej Slimák <ondrej.slimak@riganti.cz>
2021-11-21 21:31:19 +0100

    add namespace filler result

---
+++ src/HUGs/HUGs.Generator.DDD.IntegrationTests/TestResults/DddSourceGeneratorTests.ComplexDddModel_GeneratedCorrectly-namespace.cs
@@ -0,0 +1,15 @@
+namespace HUGs.DDD.Generated.Entity
+{
+}
+
+namespace HUGs.DDD.Generated.Aggregate
+{
+}
+
+namespace HUGs.DDD.Generated.ValueObject
+{
+}
+
+namespace HUGs.DDD.Generated.Enumeration
+{
+}
