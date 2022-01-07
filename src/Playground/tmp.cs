c61c518c52bf375d6b88de43ab46ebc43b4c7bf3
Ondřej Slimák <ondrej.slimak@riganti.cz>
2022-01-07 23:16:39 +0100

    add playground tests

--- src/Playground/tmp.cs
+++ src/Playground/tmp.cs
@@ -8,46 +8,27 @@ using HUGs.DDD.Generated.Aggregate;
 using HUGs.DDD.Generated.ValueObject;
 using HUGs.DDD.Generated.Enumeration;
 
-namespace HUGs.DDD.Generated.Enumeration
-{
-    public class OrderState : HUGs.Generator.DDD.Framework.BaseModels.Enumeration
-    {
-        public static readonly OrderState Created = new(nameof(Created), "Created");
-
-        public static readonly OrderState Canceled = new(nameof(Canceled), "Canceled");
-
-        public string Name { get; }
-
-        private OrderState(string internalName, string Name)
-            : base(internalName)
-        {
-            this.Name = Name;
-        }
-
-        public static OrderState FromString(string name)
-        {
-            return name switch
-            {
-                "Created" => Created,
-                "Canceled" => Canceled,
-                _ => throw new ArgumentOutOfRangeException()
-            };
-        }
-    }
-}
-
 namespace HUGs.DDD.Generated.Entity
 {
-}
+    public class CountryId : EntityId<Country>
+    {
+        public CountryId(Guid value)
+            : base(value)
+        {
+        }
+    }
 
-namespace HUGs.DDD.Generated.Aggregate
-{
-}
+    public partial class Country : HUGs.Generator.DDD.Framework.BaseModels.Entity<Guid>
+    {
+        public string Name { get; private set; }
 
-namespace HUGs.DDD.Generated.ValueObject
-{
-}
+        public Country(CountryId id, string Name)
+        {
+            Id = id;
+            this.Name = Name;
+            OnInitialized();
+        }
 
-namespace HUGs.DDD.Generated.Enumeration
-{
+        partial void OnInitialized();
+    }
 }
