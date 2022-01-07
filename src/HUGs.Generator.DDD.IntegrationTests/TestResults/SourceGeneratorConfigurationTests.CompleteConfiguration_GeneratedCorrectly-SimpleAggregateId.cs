b8143c9a86fe979efeb7c3e60666a6f9f1932c5a
Ondřej Slimák <ondrej.slimak@riganti.cz>
2022-01-07 16:23:28 +0100

    add test results

--- src/HUGs.Generator.DDD.IntegrationTests/TestResults/SourceGeneratorConfigurationTests.CompleteConfiguration_GeneratedCorrectly-SimpleAggregateId.cs
+++ src/HUGs.Generator.DDD.IntegrationTests/TestResults/SourceGeneratorConfigurationTests.CompleteConfiguration_GeneratedCorrectly-SimpleAggregateId.cs
@@ -9,6 +9,8 @@ using My.Desired.Namespace.Entities;
 using My.Desired.Namespace.Aggregates;
 using My.Desired.Namespace.ValueObjects;
 using My.Desired.Namespace.Enumerations;
+using My.Desired.Namespace.DbEntities;
+using HUGs.DDD.Generated.Mapper;
 
 namespace My.Desired.Namespace.Aggregates
 {
