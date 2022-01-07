be2313ac6a5948576609831310fe2c2e268efde2
Ondřej Slimák <ondrej.slimak@riganti.cz>
2022-01-07 16:26:09 +0100

    add test results

--- src/HUGs.Generator.DDD.IntegrationTests/TestResults/DddSourceGeneratorTests.ComplexDddModel_GeneratedCorrectly-Address.cs
+++ src/HUGs.Generator.DDD.IntegrationTests/TestResults/DddSourceGeneratorTests.ComplexDddModel_GeneratedCorrectly-Address.cs
@@ -7,6 +7,8 @@ using HUGs.DDD.Generated.Entity;
 using HUGs.DDD.Generated.Aggregate;
 using HUGs.DDD.Generated.ValueObject;
 using HUGs.DDD.Generated.Enumeration;
+using HUGs.DDD.Generated.DbEntity;
+using HUGs.DDD.Generated.Mapper;
 
 namespace HUGs.DDD.Generated.ValueObject
 {
